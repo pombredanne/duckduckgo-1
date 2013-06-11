a52064c11714af1c2f480b2038b48b8578fdead1
Paul Furley <paul@paulfurley.com>
2013-06-11 14:58:49 +0100

    Added missing dependcies to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 setup(name='duckduckgo',
 	  version='0.1.0',
+      install_requires=['requests>=1.4.0', 'lxml>=3.2.0'],
 	  py_modules=['duckduckgo'],
 	  data_files=[('/usr/bin', ['ddg'])])
 
