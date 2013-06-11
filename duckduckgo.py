5f3d32c872665090a6d51c460e5794bace0bdeb8
Paul Furley <paul@paulfurley.com>
2013-06-11 11:26:47 +0100

    PEP8 - replaced tabs with spaces etc.

--- duckduckgo.py
+++ duckduckgo.py
@@ -2,30 +2,30 @@ import requests
 from lxml import html
 import time
 
+
 def search(keywords, max_results=None):
-	url = 'http://duckduckgo.com/html/'
-	params = {
-		'q': keywords,
-		's': '0',
-	}
+    url = 'http://duckduckgo.com/html/'
+    params = {
+        'q': keywords,
+        's': '0',
+    }
 
-	yielded = 0
-	while True:
-		res = requests.post(url, data=params)
-		doc = html.fromstring(res.text)
+    yielded = 0
+    while True:
+        res = requests.post(url, data=params)
+        doc = html.fromstring(res.text)
 
-		results = [a.get('href') for a in doc.cssselect('#links .links_main a')]
-		for result in results:
-			yield result
-			time.sleep(0.1)
-			yielded += 1
-			if max_results and yielded >= max_results:
-				return
+        results = [a.get('href') for a in
+                   doc.cssselect('#links .links_main a')]
+        for result in results:
+            yield result
+            time.sleep(0.1)
+            yielded += 1
+            if max_results and yielded >= max_results:
+                return
 
-		try:
-			form = doc.cssselect('.results_links_more form')[-1]
-		except IndexError:
-			return
-		params = dict(form.fields)
-	
-
+        try:
+            form = doc.cssselect('.results_links_more form')[-1]
+        except IndexError:
+            return
+        params = dict(form.fields)
