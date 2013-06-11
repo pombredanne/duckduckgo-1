0b22492d425aefc2e4a0930cf7971881a717453a
Paul Furley <paul@paulfurley.com>
2013-06-11 11:38:06 +0100

    Updated 'no results' test to use a random string

--- tests.py
+++ tests.py
@@ -1,11 +1,17 @@
 import unittest
 import duckduckgo
+import random
+import string
+
 
 class DuckDuckGoTest(unittest.TestCase):
     def test_detect_no_results(self):
+        random_string = "".join([
+            random.choice(string.ascii_letters) for n in xrange(30)])
+
         self.assertRaises(
             duckduckgo.NoResultsException,
-            lambda: duckduckgo.search('"noresultsshouldbefound"'))
+            lambda: duckduckgo.search("\"%s\"" % random_string))
 
     def test_some_results(self):
         results = list(duckduckgo.search('test', max_results=5))
